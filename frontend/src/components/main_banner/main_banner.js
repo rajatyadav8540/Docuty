ea47ac72629e6740c3ba7e46d0327f9716f55fa2
a-s-t-e-y-a <krishnoit100@gmail.com>
2022-09-13 10:35:51 +0530

    main banner capsule part done

--- frontend/src/components/main_banner/main_banner.js
+++ frontend/src/components/main_banner/main_banner.js
@@ -1,34 +1,21 @@
-
-
-import doc from './doc.png'
-export default function MainBanner  ({
-  message,
-  buttonName
-}){
-        
-            return(
-              <>    
-              
-              <div className='bg-green-500 my-24 mx-32 rounded-3xl flex justify-between px-24 items-center'>
-                <div className=''>
-                    <div>
-                      <h1 className='text-4xl font-semibold mb-4'>
-                        {message}
-                      </h1>
-                      <div>
-                        <button className='text-3xl text-white bg-black px-8 py-4 rounded-lg hover:bg-white hover:text-black'>
-                          {buttonName} 
-                        </button>
-                      </div>
-                    </div>
-                </div>
-                <div className='-mt-20'>
-                    <img src={doc} alt="logo" className=''/>
-                </div>
-              </div>
-              
-              
-             </>
-            )
-      
+import doc from "./doc.png";
+export default function MainBanner({ message, buttonName }) {
+  return (
+    <>
+      <div className="flex justify-center mx-32 my-12 gap-12">
+        <div>
+          <img
+            src="https://images.unsplash.com/photo-1504813184591-01572f98c85f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"
+            className="rounded-l-full brightness-75"
+          ></img>
+        </div>
+        <div>
+          <img
+            src="https://images.unsplash.com/photo-1504813184591-01572f98c85f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"
+            className="rounded-r-full brightness-75"
+          ></img>
+        </div>
+      </div>
+    </>
+  );
 }
--- frontend/src/components/signup_login/signup.js
+++ frontend/src/components/signup_login/signup.js
@@ -1,44 +1,35 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { signupFields } from "../../constants/formfield";
 import Input from "./input";
 
-const fields=signupFields;
+const fields = signupFields;
 let fieldsState = {};
-fields.forEach(field=>fieldsState[field.id]='');
+fields.forEach((field) => (fieldsState[field.id] = ""));
 
-export default function Signup(){
-    const [signupState,setsignupState]=useState(fieldsState);
+export default function Signup() {
+  const [signupState, setsignupState] = useState(fieldsState);
 
-    const handleChange=(e)=>{
-        setsignupState({...signupState,[e.target.id]:e.target.value})
-       
-    }
+  const handleChange = (e) => {
+    setsignupState({ ...signupState, [e.target.id]: e.target.value });
+  };
 
-
-    return(
-        <form className="mt-8 space-y-6">
-        <div className="space-y-px">
-            {
-                fields.map(field=>
-                        <Input
-                            key={field.id}
-                            handleChange={handleChange}
-                            value={signupState[field.id]}
-                            labelText={field.labelText}
-                            labelFor={field.labelFor}
-                            id={field.id}
-                            name={field.name}
-                            type={field.type}
-                            isRequired={field.isRequired}
-                           
-                    />
-                
-                )
-            }
-        </div>
-
-       
-
-      </form>
-    )
+  return (
+    <form className="mt-8 space-y-6">
+      <div className="space-y-px">
+        {fields.map((field) => (
+          <Input
+            key={field.id}
+            handleChange={handleChange}
+            value={signupState[field.id]}
+            labelText={field.labelText}
+            labelFor={field.labelFor}
+            id={field.id}
+            name={field.name}
+            type={field.type}
+            isRequired={field.isRequired}
+          />
+        ))}
+      </div>
+    </form>
+  );
 }
