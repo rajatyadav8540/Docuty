0c0246cf90e089990b369c44eb4c8cdccf562e24
a-s-t-e-y-a <krishnoit100@gmail.com>
2022-09-15 15:31:02 +0530

    doc card + one more section added

--- frontend/src/components/doctor_card/final_card.js
+++ frontend/src/components/doctor_card/final_card.js
@@ -1,30 +1,27 @@
-import {Doc} from './constant/docdata'
-import Template from './card_template'
-import Header from './card_header'
-const doc = Doc
-export default function DocCard(){
-    
-    return(
-        <>
-        <div className='mx-28'>
-            <Header 
-            heading='Top rated doctor'
-            subHeading ="Private online consultations with verified doctors in all specialists"
-            />
-        
-        <div className='grid grid-cols-4 gap-4 '>
-        {
-            doc.map((docy)=>(
-            <Template 
-             name={docy.name}
-             image ={docy.img}
-             content ={docy.content}
-            />
-            ))
-        }
-        </div>
-        </div>
-        </>
-    )
+import { Doc } from "./constant/docdata";
+import Template from "./card_template";
+import Header from "./card_header";
+const doc = Doc;
+export default function DocCard() {
+  return (
+    <>
+      <div className="mx-28">
+        <Header
+          heading="Top rated doctor"
+          subHeading="Private online consultations with verified doctors in all specialists"
+        />
 
+        <div className="grid grid-cols-4 gap-4 ">
+          {doc.map((docy) => (
+            <Template
+                
+              name={docy.name}
+              image={docy.img}
+              content={docy.content}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
