74103a58af45b8fddc1d67d7e6343129c7c2f4ed
a-s-t-e-y-a <krishnoit100@gmail.com>
2022-09-15 15:46:31 +0530

    jsut minor save

---
+++ frontend/src/components/testimonial/full.js
@@ -0,0 +1,13 @@
+import Template from "./template";
+export default function FullTestimonial(){
+    const review  = rev
+    return(
+        <>
+            <div>
+                {
+                    
+                }
+            </div>
+        </>
+    )
+}
