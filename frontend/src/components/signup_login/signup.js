6327291deabadb924aa0dc8031bdf13594c59f70
a-s-t-e-y-a <krishnoit100@gmail.com>
2022-09-30 23:56:06 +0530

    api integration done scussefully

--- frontend/src/components/signup_login/signup.js
+++ frontend/src/components/signup_login/signup.js
@@ -2,16 +2,35 @@ import { useState } from "react";
 import { signupFields } from "../../constants/formfield";
 import Input from "./input";
 import Formaction from "./formaction";
+import { useNavigate } from 'react-router-dom'
 const fields = signupFields;
 let fieldsState = {};
 fields.forEach((field) => (fieldsState[field.id] = ""));
+const history = useNavigate();
 
 export default function Signup() {
   const [signupState, setsignupState] = useState(fieldsState);
 
   const handleChange = (e) => {
     setsignupState({ ...signupState, [e.target.id]: e.target.value });
+    
   };
+  
+  async function signup(e){
+    e.preventDefault();
+     console.log("adtiya lund hai")
+    console.log(JSON.stringify(signupState))
+     let result =await fetch("http://127.0.0.1:8000/api/register/",{
+      method:"POST",
+      body:JSON.stringify(signupState),
+      headers:  {
+        "Content-type":"application/json",
+        "Accept":"application/json"
+      }
+     })
+     result = await result.json();
+     history("/login")
+  }
 
   return (
     <form className="mt-8 space-y-6">
@@ -30,7 +49,8 @@ export default function Signup() {
           />
         ))}
         <Formaction
-        text="Sign up"/>
+        text="Sign up"
+        handle={signup}/>
       </div>
     </form>
   );
