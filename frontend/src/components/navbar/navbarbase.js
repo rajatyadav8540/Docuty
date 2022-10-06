a1f50bda0ae3ac166e34cd67551846f08b17ef27
a-s-t-e-y-a <krishnoit100@gmail.com>
2022-10-06 19:09:22 +0530

    token stored in local storage and login and sign up navbar components also gets hide

--- frontend/src/components/navbar/navbarbase.js
+++ frontend/src/components/navbar/navbarbase.js
@@ -1,28 +1,26 @@
+import NavLogo from './navbarlogo';
 
-import NavLogo from "./navbarlogo";
-
-import Log_Sign from "./sing_login_navbar";
-
-
+import Log_Sign from './sing_login_navbar';
 
 export default function Navbar() {
-  return (
-    <div className="flex justify-between p-8 border-b-2 items-center">
-      <div>
-        <NavLogo logo="Docuty" logoUrl="/" />
-      </div>
-      <div className="flex justify-center gap-x-9 items-center">
-        <Log_Sign
-            name="Login"
-            usernameUrl="/login"
-            docnameUrl="/login_doc"
-            />
-        <Log_Sign
-            name="Sign up"
-            usernameUrl="/signup"
-            docnameUrl="/signup_doc"
-            />
-      </div>
-    </div>
-  );
+	const loggedIn = localStorage.getItem('token');
+	return (
+		<div className="flex justify-between p-8 border-b-2 items-center">
+			<div>
+				<NavLogo logo="Docuty" logoUrl="/" />
+			</div>
+			{!loggedIn ? (
+				<div className="flex justify-center gap-x-9 items-center">
+					<Log_Sign name="Login" usernameUrl="/login" docnameUrl="/login_doc" />
+					<Log_Sign
+						name="Sign up"
+						usernameUrl="/signup"
+						docnameUrl="/signup_doc"
+					/>
+				</div>
+			) : (
+				<div> </div>
+			)}
+		</div>
+	);
 }
--- frontend/src/components/signup_login/login.js
+++ frontend/src/components/signup_login/login.js
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { loginFields } from '../../constants/formfield';
 import Input from './input';
 import Formaction from './formaction';
+import { useNavigate } from 'react-router-dom';
 
 const fields = loginFields;
 let fieldsState = {};
@@ -9,14 +10,15 @@ fields.forEach((field) => (fieldsState[field.id] = ''));
 
 export default function Login() {
 	const [loginState, setLoginState] = useState(fieldsState);
-
+	const history = useNavigate();
 	const handleChange = (e) => {
 		setLoginState({ ...loginState, [e.target.id]: e.target.value });
 	};
 
-	const login = (e) => {
+	const login = async (e) => {
 		e.preventDefault();
-		const result = fetch('http://127.0.0.1:8000/api/login/', {
+		console.log(JSON.stringify(loginState));
+		let result = await fetch('http://127.0.0.1:8000/api/login/', {
 			method: 'POST',
 			body: JSON.stringify(loginState),
 			headers: {
@@ -24,6 +26,23 @@ export default function Login() {
 				Accept: 'application/json',
 			},
 		});
+		result = await result.json();
+
+		if (
+			JSON.stringify(result.username) === '["This field may not be blank."]' ||
+			JSON.stringify(result.password) === '["This field may not be blank."]'
+		) {
+			alert('Username and password field cannot be empty');
+		} else if (
+			JSON.stringify(result.non_field_errors) ===
+			'["Unable to log in with provided credentials."]'
+		) {
+			alert('Enter valid credentials');
+		} else if (result.token) {
+			console.log('congrats');
+			localStorage.setItem('token', JSON.stringify(result.token));
+			history('/');
+		}
 	};
 
 	return (
