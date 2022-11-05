d366f2c92835fd211f86248c2f74d56718fd0eeb
rajatyadav8540 <51041014+rajatyadav8540@users.noreply.github.com>
2022-11-05 14:39:21 +0530

    added extra fields

--- api/Multiuser Model/users/api/serializers.py
+++ api/Multiuser Model/users/api/serializers.py
@@ -10,9 +10,12 @@ class UserSerializer(serializers.ModelSerializer):
 
 class DoctorSignupSerializer(serializers.ModelSerializer):
     password2=serializers.CharField(style={"input_type":"password"}, write_only=True)
+    city=serializers.CharField(write_only=True)
+    experience=serializers.CharField(write_only=True)
+    speciality=serializers.CharField(write_only=True)
     class Meta:
         model=User
-        fields=['username','email','password', 'password2']
+        fields=['username','email','password', 'password2','city','experience','speciality']
         extra_kwargs={
             'password':{'write_only':True}
         }
@@ -24,12 +27,15 @@ class DoctorSignupSerializer(serializers.ModelSerializer):
         )
         password=self.validated_data['password']
         password2=self.validated_data['password2']
+        city=self.validated_data['city']
+        experience=self.validated_data['experience']
+        speciality=self.validated_data['speciality']
         if password !=password2:
             raise serializers.ValidationError({"error":"password do not match"})
         user.set_password(password)
         user.is_doctor=True
         user.save()
-        Doctor.objects.create(user=user)
+        Doctor.objects.create(user=user,city=city,experience=experience,speciality=speciality)
         return user
 
 
