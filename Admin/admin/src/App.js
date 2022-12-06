a91ffe60d8baeaf7b10d84ed0e8acc01895582ac
a-s-t-e-y-a <krishnoit100@gmail.com>
2022-12-06 19:43:56 +0530

    firebase connection establish

--- Admin/admin/src/App.js
+++ Admin/admin/src/App.js
@@ -1,11 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Card_body from "./components/cards/card_full_body";
 import Footer from "./components/footer/footer_template.js";
 import Card from "./components/all_store_card/full_card.js";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Store from "./components/cards/card_store";
+import firebase from "firebase";
+import React from "react";
 function App() {
+  const firebaseApp = firebase.apps[0];
   return (
     <div className="App">
       <BrowserRouter>
@@ -16,6 +18,9 @@ function App() {
         </Routes>
       </BrowserRouter>
       <Footer />
+      <code>
+        <pre>{JSON.stringify(firebaseApp.options, null, 2)}</pre>
+      </code>
     </div>
   );
 }
--- Admin/admin/src/index.js
+++ Admin/admin/src/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAhV4vJAkLcQ9U_kk2OtTXxDyMTHp0WlD0",
+  authDomain: "taffy-a9c2d.firebaseapp.com",
+  databaseURL: "https://taffy-a9c2d-default-rtdb.firebaseio.com",
+  projectId: "taffy-a9c2d",
+  storageBucket: "taffy-a9c2d.appspot.com",
+  messagingSenderId: "448381700196",
+  appId: "1:448381700196:web:3d267fba428d6275dcb771",
+  measurementId: "G-NPTPY1V9E4",
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
